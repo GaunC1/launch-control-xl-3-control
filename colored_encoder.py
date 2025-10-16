933cd03c9033a37c9b361eeb33e707b810237843
Gaun Cochran <Gauncochran@gmail.com>
2025-10-16 09:15:28 +0800

    Page 1: set column 4 to palette index 31 via Rgb.from_index(31).

--- colored_encoder.py
+++ colored_encoder.py
@@ -28,7 +28,7 @@ _BANK1_COLUMN_COLORS = (
     Rgb.RED,          # col 1
     Rgb.GREEN,        # col 2
     Rgb.BLUE,         # col 3
-    Rgb.from_index(125),  # col 4 (palette index 125)
+    Rgb.from_index(31),   # col 4 (palette index 31)
     Rgb.PURPLE,       # col 5
     Rgb.YELLOW,       # col 6
     Rgb.YELLOW,       # col 7
