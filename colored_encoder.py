4163fd0f59f008cd7e6c5dec04fe9facfb8a79d7
Gaun Cochran <Gauncochran@gmail.com>
2025-10-16 09:10:16 +0800

    Page 1 color tweak: set column 4 to Rgb.BROWN (palette index 11).

--- colored_encoder.py
+++ colored_encoder.py
@@ -28,7 +28,7 @@ _BANK1_COLUMN_COLORS = (
     Rgb.RED,          # col 1
     Rgb.GREEN,        # col 2
     Rgb.BLUE,         # col 3
-    Rgb.DARK_BLUE,    # col 4
+    Rgb.BROWN,        # col 4 (changed to Brown)
     Rgb.PURPLE,       # col 5
     Rgb.YELLOW,       # col 6
     Rgb.YELLOW,       # col 7
